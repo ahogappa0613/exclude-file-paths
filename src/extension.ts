ebab3279ff59b56d2510af3ba365c93461a8941b
ahogappa <ahogappa@gmail.com>
2021-02-25 20:10:43 +0900

    0.0.1

--- src/extension.ts
+++ src/extension.ts
@@ -13,15 +13,20 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('exclude-file-paths.helloWorld', () => {
+	let disposable = vscode.commands.registerCommand('exclude-file-paths.invoke', () => {
 		// The code you place here will be executed every time your command is executed
 
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from exclude file paths!');
+		async () => {
+			await vscode.commands.executeCommand("workbench.action.findInFiles", {
+				query: "",
+				filesToExclude: vscode.workspace.getConfiguration('excludeFilePaths').pathPattern,
+			});
+		};
 	});
 
 	context.subscriptions.push(disposable);
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
