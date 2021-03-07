1c76b356cac9811e6844b2232dadb89d8dffc004
ahogappa <ahogappa@gmail.com>
2021-03-08 01:44:22 +0900

    copy clipboard

--- src/extension.ts
+++ src/extension.ts
@@ -13,15 +13,10 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('exclude-file-paths.invoke', () => {
+	let disposable = vscode.commands.registerCommand('exclude-file-paths.invoke', async () => {
+		await addExcludeFilePaths();
 		// The code you place here will be executed every time your command is executed
-		// const git = gitExtension.getAPI(1);
 		// Display a message box to the user
-		// vscode.window.showInformationMessage(vscode.workspace.getConfiguration('excludeFilePaths').pathPattern.join(", "));
-		vscode.commands.executeCommand("workbench.action.findInFiles", {
-			query: "",
-			filesToExclude: vscode.workspace.getConfiguration('excludeFilePaths').pathPattern.join(", "),
-		});
 	});
 
 	context.subscriptions.push(disposable);
@@ -29,3 +24,12 @@ export function activate(context: vscode.ExtensionContext) {
 
 // this method is called when your extension is deactivated
 export function deactivate() { }
+
+async function addExcludeFilePaths() {
+	const clip = await vscode.env.clipboard.readText();
+	// vscode.window.showInformationMessage(vscode.workspace.getConfiguration('excludeFilePaths').pathPattern.join(", "));
+	vscode.commands.executeCommand("workbench.action.findInFiles", {
+		query: clip,
+		filesToExclude: vscode.workspace.getConfiguration('excludeFilePaths').pathPattern.join(", "),
+	});
+}
